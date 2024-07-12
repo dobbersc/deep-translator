abde0983b812af78640194c5d384298b7b86f76d
dobbersc <conrad.dobberstein@informatik.hu-berlin.de>
2024-07-12 17:26:55 +0200

    Add `CACHE_DIRECTORY` variable as file cache location

--- src/translator/__init__.py
+++ src/translator/__init__.py
@@ -1,1 +1,7 @@
-__version__: str = "0.0.1"
+import os
+from pathlib import Path
+from typing import Final
+
+__version__: Final[str] = "0.0.1"
+
+CACHE_DIRECTORY: Path = Path(os.getenv("DEEP_TRANSLATOR_CACHE") or Path.home() / ".cache" / "deep_translator")
