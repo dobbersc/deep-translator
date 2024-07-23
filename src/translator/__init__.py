c54cfcbbe5554f805733b11d974c27bdff32a9e4
dobbersc <conrad.dobberstein@informatik.hu-berlin.de>
2024-07-24 00:56:57 +0200

    Add `Translator` to root

--- src/translator/__init__.py
+++ src/translator/__init__.py
@@ -2,6 +2,10 @@ import os
 from pathlib import Path
 from typing import Final
 
+from ._translator import Translator
+
+__all__ = ["__version__", "CACHE_DIRECTORY", "Translator"]
+
 __version__: Final[str] = "0.0.1"
 
 CACHE_DIRECTORY: Path = Path(os.getenv("DEEP_TRANSLATOR_CACHE") or Path.home() / ".cache" / "deep_translator")
