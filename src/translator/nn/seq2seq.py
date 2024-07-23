10a3351c43d7ca32d0e7e9e7b9830bd45001497e
dobbersc <conrad.dobberstein@informatik.hu-berlin.de>
2024-07-24 00:56:05 +0200

    Implement sequence to sequence model

---
+++ src/translator/nn/seq2seq.py
@@ -0,0 +1,19 @@
+from torch import Tensor
+from translator.nn.decoder import DecoderLSTM
+from translator.nn.encoder import EncoderLSTM
+from translator.nn.module import Module
+
+
+class Seq2Seq(Module):
+    def __init__(self, encoder: EncoderLSTM, decoder: DecoderLSTM) -> None:
+        super().__init__()
+
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, sources: Tensor, targets: Tensor, teacher_forcing_ratio: float = 0.5) -> Tensor:
+        encoder_hidden_states, encoder_hidden_and_cell = self.encoder(sources)
+        return self.decoder(targets, encoder_hidden_states, encoder_hidden_and_cell, teacher_forcing_ratio)
+
+    def generate(self, sources: Tensor) -> Tensor:
+        pass
