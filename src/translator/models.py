68f5bff7ccab1778515bdfabc275e3f50563247a
dobbersc <conrad.dobberstein@informatik.hu-berlin.de>
2024-07-24 19:44:26 +0200

    Move translator to `models.py`

---
+++ src/translator/models.py
@@ -0,0 +1,121 @@
+from collections.abc import Sequence, Callable
+from typing import Protocol, Iterable
+
+import math
+import torch
+from gensim.models import KeyedVectors
+from torch import Tensor
+from tqdm import tqdm
+from translator.language import Language
+from translator.nn import DecoderLSTM, EncoderLSTM, Seq2Seq
+from translator.preprocessing import Tokenizer
+
+from datasets import VectorizedDataPointBatch
+
+
+class Detokenizer(Protocol):
+    def __call__(self, tokens: Sequence[str]) -> str:
+        ...
+
+
+class Translator(Seq2Seq):
+    # Implement searches and complete pipeline here.
+    # This should be the easy-to-use entry point
+
+    def __init__(
+        self,
+        *,
+        source_language: Language,
+        target_language: Language,
+        source_tokenizer: Tokenizer,
+        target_tokenizer: Tokenizer,
+        target_detokenizer: Detokenizer = lambda x: " ".join(x),
+        source_embedding_size: int = 256,
+        target_embedding_size: int = 256,
+        hidden_size: int = 512,
+        attention: None = None,  # noqa: ARG002
+        source_pretrained_embeddings: KeyedVectors | Tensor | None = None,
+        target_pretrained_embeddings: KeyedVectors | Tensor | None = None,
+        freeze_pretrained_embeddings: bool = True,
+        embedding_dropout: float = 0,
+        dropout: float = 0,
+    ) -> None:
+        super().__init__(
+            EncoderLSTM(
+                vocabulary_size=source_language.vocabulary_size,
+                embedding_size=source_embedding_size,
+                hidden_size=hidden_size,
+                pretrained_embeddings=source_pretrained_embeddings,
+                freeze_pretrained_embeddings=freeze_pretrained_embeddings,
+                embedding_dropout=embedding_dropout,
+                dropout=dropout,
+                padding_index=source_language.padding_token_index,
+                start_index=source_language.start_token_index,
+            ),
+            DecoderLSTM(
+                vocabulary_size=target_language.vocabulary_size,
+                embedding_size=target_embedding_size,
+                hidden_size=hidden_size,
+                pretrained_embeddings=target_pretrained_embeddings,
+                freeze_pretrained_embeddings=freeze_pretrained_embeddings,
+                embedding_dropout=embedding_dropout,
+                dropout=dropout,
+                padding_index=target_language.padding_token_index,
+                seperator_index=target_language.seperator_token_index,
+                stop_index=target_language.stop_token_index,
+            ),
+        )
+
+        self.source_language = source_language
+        self.target_language = target_language
+
+        self.source_tokenizer = source_tokenizer
+        self.target_tokenizer = target_tokenizer
+        self.target_detokenizer = target_detokenizer
+
+    def translate(self, text: str) -> str:
+        raise NotImplementedError
+
+    @torch.no_grad()
+    def evaluate(
+            self,
+            data_points: Iterable[VectorizedDataPointBatch],
+            criterion: Callable[[Tensor, Tensor], Tensor] | None = None,
+    ) -> tuple[float, float]:
+        self.eval()
+
+        if criterion is None:
+            criterion = torch.nn.NLLLoss(ignore_index=self.decoder.padding_index)
+
+        nll_criterion: torch.nn.NLLLoss = torch.nn.NLLLoss(ignore_index=self.decoder.padding_index)
+
+        # Keep track of running loss
+        validation_loss: float = 0
+        validation_nll_loss: float = 0
+
+        num_batches: int = 0
+        for data_point_batch in tqdm(data_points, desc="Evaluating Model", unit="batch", leave=False):
+            # Move data to the model's device
+            sources: Tensor = data_point_batch.sources.to(self.device)
+            targets: Tensor = data_point_batch.targets.to(self.device)
+            ground_truth: Tensor = self.decoder.make_ground_truth_sequences(targets).flatten()
+
+            # Evaluate the model with teacher forcing disabled
+            # Using the model, compute the log probabilities and loss
+            predicted_log_probabilities: Tensor = self(sources, targets, teacher_forcing_ratio=0)
+
+            loss: Tensor = criterion(predicted_log_probabilities.flatten(end_dim=1), ground_truth)
+            nll_loss: Tensor = nll_criterion(predicted_log_probabilities.flatten(end_dim=1), ground_truth)
+
+            validation_loss += loss.item()
+            validation_nll_loss += nll_loss.item()
+            num_batches += 1
+
+        validation_loss /= num_batches
+        validation_nll_loss /= num_batches
+        validation_perplexity: float = math.exp(validation_nll_loss)
+
+        return validation_loss, validation_perplexity
+
+    def evaluate_bleu(self, sources: Sequence[str], targets: Sequence[str], batch_size: int = 32) -> float:
+        raise NotImplementedError
